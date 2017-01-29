8dad56602c06c26471dc38f1db2dc88a11b886c5
Pavlo Grijincu <pavlo.grijincu@gmail.com>
2017-01-28 19:57:13 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+###Blender4J - Grind Java to Pieces
+
+Getting the syntax tree using the javaparser library, and importing it into Neo4j for some analysis.
---
+++ src/main/java/gr/pavlo/blender4j/Blender.java
@@ -0,0 +1,10 @@
+package gr.pavlo.blender4j;
+
+/**
+ * Created by Pavlo on 1/23/2017.
+ */
+public class Blender {
+    public static void main(String[] args) {
+        System.out.println("But will it blend?");
+    }
+}
